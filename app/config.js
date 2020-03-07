cf7bad6b5006ab25f63218fb72e401c72af042b4
unknown <Administrator@DEMO.NETAPP.COM>
2020-03-07 00:11:14 +0000

    Updated to be helm installed

--- app/config.js
+++ app/config.js
@@ -21,6 +21,13 @@ module.exports = {
         name: "scotch-user-map-local",
         url: "mongodb://localhost/MeanMapAppTest",
         port: 27017
+    },
+
+    bitnami:
+    {
+        name: "MongoDB Service",
+        url: "mongodb://" + process.env.DATABASE_USERNAME + ":" + process.env.DATABASE_PASSWORD + "@" + process.env.DATABASE_HOST + "/" + process.env.DATABASE_NAME,
+        port: process.env.DATABASE_PORT
     }
 
 };
